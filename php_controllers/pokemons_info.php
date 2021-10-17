7b7804fa9bb57aed8a30216df7792be47a2a3842
Isipisi00 <gonzalezaparicio.2000@gmail.com>
2021-10-17 15:09:25 +0200

    Structure creation

---
+++ php_controllers/pokemons_info.php
@@ -0,0 +1,11 @@
+<?php
+
+if (isset($_POST)){
+
+    $name =     $_POST['pokemon_name'];
+    $region =   $_POST['pokemon_region'];
+    $type =     $_POST['pokemon_type'];
+    $height =   $_POST['pokemon_height'];
+    $weight =   $_POST['pokemon_weight'];
+    $evo =      $_POST['pokemon_evolution'];
+}
---
+++ php_views/pokemon_create.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    <form action="../php_libraries/pokemons_info.php" method="post">
+        <input type="text" name="pokemon_name">
+        <input type="text" name="pokemon_region">
+        <input type="text" name="pokemon_type">
+        <input type="number" name="pokemon_height">
+        <input type="number" step="0.01" name="pokemon_weight">
+        <input type="text" name="pokemon_evolution">
+    </form>
+</body>
+</html>
